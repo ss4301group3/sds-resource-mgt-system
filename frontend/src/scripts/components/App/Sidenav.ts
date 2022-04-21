c1db9b663cfbe2e4ea4ebb5f6b9605883e74efa6
unknown <wafibismail@gmail.com>
2022-04-21 20:43:50 +0800

    Fixed faulty css for Sidenav behaviour

--- frontend/src/scripts/components/App/Sidenav.ts
+++ frontend/src/scripts/components/App/Sidenav.ts
@@ -1,16 +1,13 @@
 import { ElemGetter, getOrCreate, ifClicked, on } from "../../utils/html";
 import "../../../stylesheets/components/App/Sidenav.scss";
-import { MainContainer } from "./MainContainer";
-import { MessageBox } from "./MessageBox";
 
 class SidenavStatus {
-    static OPTIONS = { collapsed: "collapsed", partial: "partial", total: "total" }
+    static OPTIONS = { collapsed: "collapsed", expanded: "expanded" }
 
     static setTo() {
         return {
             collapsed() { status = SidenavStatus.OPTIONS.collapsed; Sidenav.effect(); },
-            partial() { status = SidenavStatus.OPTIONS.partial; Sidenav.effect(); },
-            total() { status = SidenavStatus.OPTIONS.total; Sidenav.effect();  },
+            expanded() { status = SidenavStatus.OPTIONS.expanded; Sidenav.effect(); }
         }
     }
 
@@ -18,18 +15,9 @@ class SidenavStatus {
 
     static toggle() {
         const collapsed = this.get() == this.OPTIONS.collapsed;
-        const partiallyOpened = this.get() == this.OPTIONS.partial;
-        const totallyOpened = this.get() == this.OPTIONS.total;
 
-        if(window.innerWidth < 675) {
-            if(!totallyOpened) this.setTo().total();
-            else this.setTo().collapsed();
-        }
-        else {
-            if(!partiallyOpened) this.setTo().partial();
-            else this.setTo().collapsed();
-        }
-        console.log(SidenavStatus.get())
+        if(collapsed) this.setTo().expanded();
+        else this.setTo().collapsed();
     }
 }
 let status: string = SidenavStatus.OPTIONS.collapsed;
@@ -86,19 +74,11 @@ export class Sidenav {
 
         if(status == SidenavStatus.OPTIONS.collapsed) {
             for(let i = 0; i < effectedElems.length; i++) {
-                effectedElems[i].classList.remove("sidenav-toggle-partial", "sidenav-toggle-total");
+                effectedElems[i].classList.remove("sidenav-toggle-active");
             }
-        }
-        if(status == SidenavStatus.OPTIONS.partial) {
+        } else {
             for(let i = 0; i < effectedElems.length; i++) {
-                effectedElems[i].classList.add("sidenav-toggle-partial");
-                effectedElems[i].classList.remove("sidenav-toggle-total");
-            }
-        }
-        if(status == SidenavStatus.OPTIONS.total) {
-            for(let i = 0; i < effectedElems.length; i++) {
-                effectedElems[i].classList.add("sidenav-toggle-total");
-                effectedElems[i].classList.remove("sidenav-toggle-partial");
+                effectedElems[i].classList.add("sidenav-toggle-active");
             }
         }
     }
