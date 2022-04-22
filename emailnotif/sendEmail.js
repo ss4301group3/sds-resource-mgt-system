a97779ef1fc8434ab84086a597805d047f77b533
18b9007 <98511395+18b9007@users.noreply.github.com>
2022-04-22 19:14:28 +0800

    add send function for different types of email

--- emailnotif/sendEmail.js
+++ emailnotif/sendEmail.js
@@ -1,16 +1,45 @@
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 
-const CLIENT_ID = 'client id';
-const CLIENT_SECRET = 'client secret';
+const CLIENT_ID = 'client ID here';
+const CLIENT_SECRET = 'client secret here';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = 'refresh token';
+const REFRESH_TOKEN =  'refresh token from oauth playground here';
 
-const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+const sender ='sender email here';
+const recipient = 'recepient email here';
+const itemName = 'HP Monitor'; // itemName from dbo.RESERVATION
 
+const approval = {
+  from: sender,
+  to: recipient,
+  subject: 'APPROVED: SDS Resource Reservation',
+  text: `Dear User,</p><p>Your resource reservation for ${itemName} was approved.`,
+  html: `<p>Dear User,</p><p>Your resource reservation for ${itemName} was rejected.</p>`,
+};
+
+const reject = {
+  from: sender,
+  to: recipient,
+  subject: 'REJECTED: SDS Resource Reservation',
+  text: `Dear User,</p><p>Your resource reservation for ${itemName} was rejected.`,
+  html: `<p>Dear User,</p><p>Your resource reservation for ${itemName} was rejected.</p>`,
+};
+
+const newReq = {
+  from: sender,
+  to: sender,
+  subject: 'NEW: Resource Reservation',
+  text: `Dear Admin,</p><p>There is a new reservation for ${itemName}. Please visit the portal to respond to it.`,
+  html: `<p>Dear Admin,</p><p>There is a new reservation for ${itemName}. Please visit the portal to respond to it.</p>`,
+};
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-// import email arrays from collectEmail
 
 async function sendMail() {
   try {
@@ -20,7 +49,7 @@ async function sendMail() {
       service: 'gmail',
       auth: {
         type: 'OAuth2',
-        user: 'sender email',
+        user: sender,
         clientId: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
         refreshToken: REFRESH_TOKEN,
@@ -28,15 +57,7 @@ async function sendMail() {
       },
     });
 
-    const mailOptions = {
-      from: 'sender email',
-      to: 'recipient email array',
-      subject: '',
-      text: '',
-      html: '',
-    };
-
-    const result = await transport.sendMail(mailOptions);
+    const result = await Promise.all([transport.sendMail(approval), transport.sendMail(reject), transport.sendMail(newReq)])
     return result;
   } catch (error) {
     return error;
@@ -44,5 +65,5 @@ async function sendMail() {
 }
 
 sendMail()
-  .then((result) => console.log('Email sent...', result))
+  .then((result) => console.log('Email sent!', result))
   .catch((error) => console.log(error.message));
