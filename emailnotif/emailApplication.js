19d49395bd57aad65802fd9cf47a1a635a1c61fd
18b9007 <98511395+18b9007@users.noreply.github.com>
2022-04-24 02:52:41 +0800

    refactor code and mock data

--- emailnotif/emailApplication.js
+++ emailnotif/emailApplication.js
@@ -1,68 +1,58 @@
+/* ----------------------------  collectEmail and sendEmail combined  ------------------------ */
 const fs = require('fs');
 
-const reservation = fs.readFileSync('reservation.csv', 'utf8')
-  .split("\r\n").filter(x => x.length > 0).map(x => x.split(","));
+const reservations = fs.readFileSync('mockdata.csv', 'utf8').split("\r\n").filter(x => x.length > 0).map(x => x.split(","));
 
-// Notify approval: isReturned (F), isPending (F), isApproved3 (T)
-const approvedEmails = [];
-function getApproved(row) {
-  if (row[7] == 'false' && row[14] == 'false' && row[17] == 'true'){
-    approvedEmails.push(row[9]);
+const approvalEmails = [];
+function getApprovalEmail(row) {
+  if (row[7] == 'FALSE' && row[14] == 'FALSE' && row[17] == 'TRUE'){
+    approvalEmails.push(row[9]);
   }
 }
 
-// Notify rejection: isPending (F), isApproved3 (F)
-const rejectEmails = [];
+const rejectionEmails = [];
 function getRejected(row) {
-  if (row[14] == 'false' && row[17] == 'false'){
-    rejectEmails.push(row[9]);
+  if (row[14] == 'FALSE' && row[17] == 'FALSE'){
+    rejectionEmails.push(row[9]);
   }
 }
 
-// Notify near deadline: isReturned (F), isPending (F), isApproved3 (T)
-const now = Date.parse(new Date());
-const day = 86400*1000;
-var endTime;
-let difference = now - endTime;
+const nowInMilliseconds = Date.parse(new Date());
+const dayInMilliseconds = 86400*1000;
+let endTime;
+let difference = nowInMilliseconds - endTime;
 
-const deadlineEmails = [];
-function nearDeadline(difference, day){
-  if (difference <= 4*day)
-  { return true; }
+function deadlineAndPresentTime(difference, dayInMilliseconds){
+  if ((difference <= 4*dayInMilliseconds) && difference > 0){
+    return true; 
+  }
 }
 
 function formatEndTime(row){
    endTime = Date.parse(new Date(row[6]));
-   difference = now - endTime;
+   difference = nowInMilliseconds - endTime;
 }
 
-function filterDeadlineEmails(row){
-  if (row[7] == 'false' && row[14] == 'false' && row[17] == 'true'){
-    formatEndTime(row);
-    if ((nearDeadline(difference, day) == true))
-    { deadlineEmails.push(row[9]); }
-  }
-}
-
+const deadlineEmails = [];
 function getDeadlineEmails(row){
-  if (row[7] == 'false' && row[14] == 'false' && row[17] == 'true'){ 
+  if (row[7] == 'FALSE' && row[14] == 'FALSE' && row[17] == 'TRUE'){ 
     formatEndTime(row);
-    filterDeadlineEmails(row);
+    if ((deadlineAndPresentTime(difference, dayInMilliseconds) == true)){
+      deadlineEmails.push(row[9]); 
+    }
   }
 }
 
-// New reservation: IsApproved1-3 (F), isPending (T)
-const newReservationIds = [];
+const newReservationEmails = [];
 function getNewReservations(row) {
-  if(row[15] == 'false' && row[16] == 'false' && row[17] == 'false' && row[14] == 'true'){
-    newReservationIds.push(row[9]);
+  if(row[15] == 'FALSE' && row[16] == 'FALSE' && row[17] == 'FALSE' && row[14] == 'TRUE'){
+    newReservationEmails.push(row[9]);
   }
 }
 
-// Perform above functions in all the reservations
-for (let i = 1; i < reservation.length; i++){
-  const row = reservation[i];
-  getApproved(row);
+for (let i = 1; i < reservations.length; i++){
+  const row = reservations[i];
+  getApprovalEmail(row);
   getRejected(row);
   getDeadlineEmails(row);
   getNewReservations(row);
@@ -71,36 +61,44 @@ for (let i = 1; i < reservation.length; i++){
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 
-const CLIENT_ID = 'API credentials client ID';
-const CLIENT_SECRET = 'API credentials client secret';
+const CLIENT_ID = 'copy client ID from your Google Cloud API Credentials';
+const CLIENT_SECRET = 'copy client secret from your Google Cloud API Credentials';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN =  'refresh token from OAuth playground';
+const REFRESH_TOKEN =  'copy refresh token from OAuth Playground authorization of GMail API with your credentials';
 
 const sender ='sender email';
-const recipient = 'not needed. email arrays can be directly passed in below functions';
+const recipient = 'recipient email';  // temporarily used to be able to email a valid address
 
 const approval = {
   from: sender,
   to: recipient,
   subject: 'APPROVED: SDS Resource Reservation',
-  text: `Reservations by these emails have been approved: ${approvedEmails}`,
-  html: `Reservations by these emails have been approved:<br>${approvedEmails}`,
+  text: `Reservations by these emails have been approved: ${approvalEmails}. An array can be passed in place of the recipient variable. This will send the email to all of them`,
+  html: `Reservations by these emails have been approved:${approvalEmails}<br>An array can be passed in place of the recipient variable. This will send the email to all of them.`,
 };
 
 const reject = {
   from: sender,
   to: recipient,
   subject: 'REJECTED: SDS Resource Reservation',
-  text: `Reservations by these emails have been rejected: ${rejectEmails}`,
-  html: `Reservations by these emails have been rejected:<br>${rejectEmails}`,
+  text: `Reservations by these emails have been rejected: ${rejectionEmails}. An array can be passed in place of the recipient variable. This will send the email to all of them.`,
+  html: `Reservations by these emails have been rejected: ${rejectionEmails}<br>An array can be passed in place of the recipient variable. This will send the email to all of them.`,
 };
 
 const newReq = {
   from: sender,
   to: sender,
   subject: 'NEW: Resource Reservation',
-  text: `Dear Admins, you have new reservations pending approval from these emails: ${newReservationIds}`,
-  html: `Dear Admins,<br>You have new reservations pending approval from these emails: ${newReservationIds}`,
+  text: `Dear Admins, you have new reservations pending approval from these emails: ${newReservationEmails}. Email of the requestor is not so helpful in this case. More meaningful reservation data could be sent to the admin/approvers such as reservationId, itemName, startTime (in case urgent).`,
+  html: `Dear Admins,<br>You have new reservations pending approval from these emais: ${newReservationEmails}<br>Email of the requestor is not so helpful in this case. More meaningful reservation data could be sent to the admin/approvers such as reservationId, itemName, startTime (in case urgent).`,
+};
+
+const deadline = {
+  from: sender,
+  to: recipient,
+  subject: 'REMINDER: Resource Reservation ends in 4 days',
+  text: `Reservations by these emails are due to end in 4 days: ${deadlineEmails}. Would be useful to inform the recipient of their exact return dates.`,
+  html: `Reservations by these emails are due to end in 4 days: ${deadlineEmails}<br>Would be useful to inform the recipient of their exact return dates.`,
 };
 
 const oAuth2Client = new google.auth.OAuth2(
@@ -126,7 +124,7 @@ async function sendMail() {
       },
     });
 
-    const result = await Promise.all([transport.sendMail(approval), transport.sendMail(reject), transport.sendMail(newReq)])
+    const result = await Promise.all([transport.sendMail(approval), transport.sendMail(reject), transport.sendMail(newReq), transport.sendMail(deadline)])
     return result;
   } catch (error) {
     return error;
--- emailnotif/sendEmail.js
+++ emailnotif/sendEmail.js
@@ -1,37 +1,44 @@
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 
-const CLIENT_ID = 'client ID here';
-const CLIENT_SECRET = 'client secret here';
+const CLIENT_ID = 'copy client ID from your Google Cloud API Credentials';
+const CLIENT_SECRET = 'copy client secret from your Google Cloud API Credentials';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN =  'refresh token from oauth playground here';
+const REFRESH_TOKEN =  'copy refresh token from OAuth Playground authorization of GMail API with your credentials';
 
-const sender ='sender email here';
-const recipient = 'recepient email here';
-const itemName = 'HP Monitor'; // itemName from dbo.RESERVATION
+const sender ='sender email';
+const recipient = 'recipient email';  // temporarily used to be able to email a valid address
 
 const approval = {
   from: sender,
   to: recipient,
   subject: 'APPROVED: SDS Resource Reservation',
-  text: `Dear User,</p><p>Your resource reservation for ${itemName} was approved.`,
-  html: `<p>Dear User,</p><p>Your resource reservation for ${itemName} was rejected.</p>`,
+  text: `Reservations by these emails have been approved: ${approvalEmails}. An array can be passed in place of the recipient variable. This will send the email to all of them`,
+  html: `Reservations by these emails have been approved:${approvalEmails}<br>An array can be passed in place of the recipient variable. This will send the email to all of them.`,
 };
 
 const reject = {
   from: sender,
   to: recipient,
   subject: 'REJECTED: SDS Resource Reservation',
-  text: `Dear User,</p><p>Your resource reservation for ${itemName} was rejected.`,
-  html: `<p>Dear User,</p><p>Your resource reservation for ${itemName} was rejected.</p>`,
+  text: `Reservations by these emails have been rejected: ${rejectionEmails}. An array can be passed in place of the recipient variable. This will send the email to all of them.`,
+  html: `Reservations by these emails have been rejected: ${rejectionEmails}<br>An array can be passed in place of the recipient variable. This will send the email to all of them.`,
 };
 
 const newReq = {
   from: sender,
   to: sender,
   subject: 'NEW: Resource Reservation',
-  text: `Dear Admin,</p><p>There is a new reservation for ${itemName}. Please visit the portal to respond to it.`,
-  html: `<p>Dear Admin,</p><p>There is a new reservation for ${itemName}. Please visit the portal to respond to it.</p>`,
+  text: `Dear Admins, you have new reservations pending approval from these emails: ${newReservationEmails}. Email of the requestor is not so helpful in this case. More meaningful reservation data could be sent to the admin/approvers such as reservationId, itemName, startTime (in case urgent).`,
+  html: `Dear Admins,<br>You have new reservations pending approval from these emais: ${newReservationEmails}<br>Email of the requestor is not so helpful in this case. More meaningful reservation data could be sent to the admin/approvers such as reservationId, itemName, startTime (in case urgent).`,
+};
+
+const deadline = {
+  from: sender,
+  to: recipient,
+  subject: 'REMINDER: Resource Reservation ends in 4 days',
+  text: `Reservations by these emails are due to end in 4 days: ${deadlineEmails}. Would be useful to inform the recipient of their exact return dates.`,
+  html: `Reservations by these emails are due to end in 4 days: ${deadlineEmails}<br>Would be useful to inform the recipient of their exact return dates.`,
 };
 
 const oAuth2Client = new google.auth.OAuth2(
@@ -57,7 +64,7 @@ async function sendMail() {
       },
     });
 
-    const result = await Promise.all([transport.sendMail(approval), transport.sendMail(reject), transport.sendMail(newReq)])
+    const result = await Promise.all([transport.sendMail(approval), transport.sendMail(reject), transport.sendMail(newReq), transport.sendMail(deadline)])
     return result;
   } catch (error) {
     return error;
@@ -65,5 +72,5 @@ async function sendMail() {
 }
 
 sendMail()
-  .then((result) => console.log('Email sent!', result))
+  .then((result) => console.log('Email sent!'))
   .catch((error) => console.log(error.message));
