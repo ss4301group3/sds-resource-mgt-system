5a13f61bfa8535489313ad5074ced98866f2689d
18b9007 <98511395+18b9007@users.noreply.github.com>
2022-04-23 10:49:13 +0800

    refactor code

--- emailnotif/getDeadlineEmail.js
+++ emailnotif/getDeadlineEmail.js
@@ -3,28 +3,35 @@ const fs = require('fs');
 const reservation = fs.readFileSync('reservation.csv', 'utf8')
   .split("\r\n").filter(x => x.length > 0).map(x => x.split(","));
 
-var endTime;
+const deadlineEmails = [];
 const now = Date.parse(new Date());
 const day = 86400*1000;
+var endTime;
 let difference = now - endTime;
-const deadlineEmails = [];
+
 function nearDeadline(difference, day){
-  if (difference <= 4*day){ return true; }}
+  if (difference <= 4*day)
+  { return true; }
+}
+
 function formatEndTime(row){
    endTime = Date.parse(new Date(row[6]));
    difference = now - endTime;
 }
 
+function filterDeadlineEmails(row){
+  if (row[7] == 'false' && row[14] == 'false' && row[17] == 'true'){
+    formatEndTime(row);
+    if ((nearDeadline(difference, day) == true))
+    { deadlineEmails.push(row[9]); }
+  }
+}
+
 for (let i = 1; i < reservation.length; i++){
   const row = reservation[i];
-  if (row[7] == 'false' && row[14] == 'false' && row[17] == 'true'){ // isReturned, isPending, isApproved3
+  if (row[7] == 'false' && row[14] == 'false' && row[17] == 'true'){ 
     formatEndTime(row);
-    console.log(i, row[6]); // endTime at index 6
-    nearDeadline(difference,day);
-    if ((nearDeadline(difference, day) == true)){
-      deadlineEmails.push(row[9]); // email at index 9
-      console.log(i, row[9]);
-    }
+    filterDeadlineEmails(row);
   }
 }
 console.log(deadlineEmails);
